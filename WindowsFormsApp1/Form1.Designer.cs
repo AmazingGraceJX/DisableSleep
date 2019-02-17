78873f28c1fef95d3fda91d576d5d67a84a6d7f0
AmazingGraceJX <34985608+AmazingGraceJX@users.noreply.github.com>
2019-02-17 20:29:35 +0800

    first commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace DisableSleepForm
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.button1 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.startToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.qiutToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(56, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Disable Sleep";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.Click += new System.EventHandler(this.NotifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem1,
+            this.stopToolStripMenuItem1,
+            this.qiutToolStripMenuItem1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(103, 70);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.ContextMenuStrip1_Opening);
+            // 
+            // startToolStripMenuItem1
+            // 
+            this.startToolStripMenuItem1.Name = "startToolStripMenuItem1";
+            this.startToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.startToolStripMenuItem1.Text = "start";
+            this.startToolStripMenuItem1.Click += new System.EventHandler(this.StartToolStripMenuItem_Click);
+            // 
+            // stopToolStripMenuItem1
+            // 
+            this.stopToolStripMenuItem1.Name = "stopToolStripMenuItem1";
+            this.stopToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.stopToolStripMenuItem1.Text = "stop";
+            this.stopToolStripMenuItem1.Click += new System.EventHandler(this.StopToolStripMenuItem_Click);
+            // 
+            // qiutToolStripMenuItem1
+            // 
+            this.qiutToolStripMenuItem1.Name = "qiutToolStripMenuItem1";
+            this.qiutToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.qiutToolStripMenuItem1.Text = "quit";
+            this.qiutToolStripMenuItem1.Click += new System.EventHandler(this.QuitToolStripMenuItem_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(244, 121);
+            this.Controls.Add(this.button1);
+            this.Name = "FormMain";
+            this.Text = "SleepKiller";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem qiutToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,91 @@
+﻿using DisableSleep;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DisableSleepForm
+{
+    public partial class FormMain : Form
+    {
+        [DllImport("kernel32.dll")]
+        static extern uint SetThreadExecutionState(uint esFlags);
+        // 选项所用到的常数
+        const uint ES_AWAYMODE_REQUIRED = 0x00000040;
+        const uint ES_CONTINUOUS = 0x80000000;
+        const uint ES_DISPLAY_REQUIRED = 0x00000002;
+        const uint ES_SYSTEM_REQUIRED = 0x00000001;
+        System.Timers.Timer pTimer;
+        Icon awakeIcon;
+        Icon sleepIcon;
+        public FormMain()
+        {
+            InitializeComponent();
+            pTimer = new System.Timers.Timer(30000);
+            pTimer.Elapsed += Timer_Elapsed;
+            pTimer.AutoReset = true;
+            awakeIcon = Resource1.Awake;
+            sleepIcon = Resource1.Asleep;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            pTimer.Enabled = true;
+            Control.CheckForIllegalCrossThreadCalls = false;
+            this.notifyIcon1.Visible = true;
+            this.WindowState = FormWindowState.Minimized;
+            this.Visible = false;
+            this.ShowInTaskbar = false;
+        }
+
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            SetThreadExecutionState(ES_CONTINUOUS | ES_DISPLAY_REQUIRED | ES_SYSTEM_REQUIRED);
+        }
+        private void NotifyIcon1_MouseDoubleClick(object sender, EventArgs e)
+        {
+            //do nothing
+        }
+        private void ContextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+            if (pTimer.Enabled == true)
+            {
+                notifyIcon1.Icon = awakeIcon;
+                startToolStripMenuItem1.Enabled = false;
+                stopToolStripMenuItem1.Enabled = true;
+            }
+            else if (pTimer.Enabled == false)
+            {
+                notifyIcon1.Icon = sleepIcon;
+                startToolStripMenuItem1.Enabled = true;
+                stopToolStripMenuItem1.Enabled = false;
+            }
+            else
+            {
+                startToolStripMenuItem1.Enabled = true;
+                stopToolStripMenuItem1.Enabled = true;
+            }
+        }
+        private void QuitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pTimer.Enabled = false;
+            this.Close();
+        }
+        private void StartToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pTimer.Enabled = true;
+            notifyIcon1.Icon = awakeIcon;
+        }
+        private void StopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pTimer.Enabled = false;
+            notifyIcon1.Icon = sleepIcon;
+        }
+    }
+}
